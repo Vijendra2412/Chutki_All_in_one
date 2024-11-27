dffb37ee0cf81eb5fec0cfb85caabe8eb926d2fc
Vijay <SharmaVijendra2412@gmail.com>
2024-11-27 10:57:04 +0530

    vector

--- ABCD/10_11_JAva/Collection.java
+++ ABCD/10_11_JAva/Collection.java
@@ -1,5 +1,7 @@
  
  import java.util.*;
+ import java.util.Vector;
+ import java.util.Iterator;
  class Collection {
      
      public static void main(String[] args) {
@@ -45,10 +47,106 @@
 
         //  System.out.println("list is:- " + num);
 
+        // ArrayList<String> lang = new ArrayList<>();
+        // lang.add("java");
+        // lang.add("Pyhton");
+        // lang.add("Swift");
+        // lang.add("C++");
+        // System.out.println("ArrayList "+lang);
+        // lang.add(3,"R");
+        // System.out.println("ArrayList "+lang);
+
+        // ArrayList<String> l1 = new ArrayList<>();
+        // l1.addAll(lang);
+        // System.out.println(l1);
+        // lang.set(2, "php");
+        // System.out.println(lang);
+
+        /*
+         size()
+         sort()
+         clone()
+         contains()
+         ensurecapacity()
+         isEmpty()
+         indexOf()
+         */
+
+         
+        // ArrayList<String> lang = new ArrayList<>();
+        // lang.add("java");
+        // lang.add("Pyhton");
+        // lang.add("Swift");
+        // lang.add("C++");
+
+        // System.out.println("ArrayList:- " + lang);
+        // String[] arr= new String[lang.size()];
+        // lang.toArray(arr);
+        // System.out.println("Array:- ");
+        // for(String item:arr){
+        //     System.out.println(item + ",");
+        // }
+
+        // ArrayList<String> lang1 = new ArrayList<>(Arrays.asList(arr));
+        // System.out.println("ArrayList:- " + lang1);
+
+        // ArrayList<String> lang = new ArrayList<>(Arrays.asList("java","pyhton","c++","c"));
+        // System.out.println(lang);
+
+ 
+        // ArrayList<String> lang = new ArrayList<>();
+        // lang.add("java");
+        // lang.add("Pyhton");
+        // lang.add("Swift");
+        // lang.add("C++");
+        // System.out.println("ArrayList:- "+lang);
+        // String str = lang.toString();
+        // System.out.println(str);
+
+
         
+        // Vector<String> lang = new Vector<>();
+        // lang.add("java");
+        // lang.add("Pyhton");
+        // lang.add("Swift");
+        // lang.add("C++");
+        // System.out.print("Vector:- "+lang);
+        // //   for(String item:lang){
+        // //     System.out.println(item + ",");
+        // // }
+
+        // Iterator<String> it = lang.iterator();
+        // while (it.hasNext()) {
+        //     System.out.print(it.next());
+        //     System.out.println(",");
+            
+        // }
+  
+        
+   
+                // Create a new Vector of Strings
+                Vector<String> lang = new Vector<>();
+                
+                // Add elements to the Vector
+                lang.add("Java");
+                lang.add("Python");
+                lang.add("Swift");
+                lang.add("C++");
+                
+                // Method 1: Using System.out.println() to print the entire Vector
+                System.out.println("Vector contents: " + lang);
+                
+                // Method 2: Using Iterator
+                System.out.println("\nIterating using Iterator:");
+                Iterator<String> it = lang.iterator();
+                while (it.hasNext()) {
+            System.out.print(it.next());
+            System.out.println(",");
+            
+        }
+            }
+        }
  
-     }
- }
- 
+      
  
  
--- ABCD/java3_4/pradeep.java
+++ ABCD/java3_4/pradeep.java
@@ -1,578 +1,71 @@
-// public class pradeep {
-//      // instance:-
-//     // static :-
-//     public static void main(String[] args) {
-    /*
-     Variable:- it is a container which store the value while the program is excuted
-
-     DataType variable_name = value
-
-     */
-    // int x =9;
-    // int y=8;
-    // int z =10;
-    // System.out.println(x);
-    // System.out.println(y);
-    // System.out.println(z);
-
-    // int x,y,z;
-    // x=9;
-    // y=12;
-    // z=2;
-
-    // int x=12,y=3,z=9;
-    //   System.out.println(x);
-    // System.out.println(y);
-    // System.out.println(z);
-
-    // int x=9;
-    //  x =8;
-    // System.out.println(X);
-
-    // types of variable 
-    // local :-
-    // instance:-
-    // static :-
-
-    // int x =9;
-    // int y=10;
-    // System.out.println(x+y);
-/* 
-    Type casting is when you assign a value of one primitive data type to another type.
-
-In Java, there are two types of casting:
-
-Widening Casting (automatically) - converting a smaller type to a larger type size
-byte -> short -> char -> int -> long -> float -> double
-
-Narrowing Casting (manually) - converting a larger type to a smaller size type
-double -> float -> long -> int -> char -> short -> byte
-*/
-
-// float x =9.8f;
-//  int y=(int)x;
-// System.out.println(x);
-// System.out.println(y);
-
-/* 
-    DataTypes            Default value            Default size
-
-    boolean :- T/F          false                   1bit
-    char :- ''            '\u0000'                  2byte
-    byte  :- number         0                       byte
-    short :-  num           0                       2byte
-    int   :                  0                      4byte
-    long                    0L                      8byte
-    float :- decimal        0.0f                    4byte
-    double:- decimal        0.0d                    8byte
-
-    
-*/
-
-// boolean x = false;
-// System.out.println(x);
-// char a = 'a';
-// System.out.println(a);
-// float b = 1.2f;
-// System.out.println(b);
-// double c = 1.3;
-// System.out.println(c);
-//     }
-// }
-
-// class a{
-//     public static void main(String[] args) {
-//         boolean x = true;
-//         System.out.println(x);
-//     }
-// }
-
-/* 
-class a{
+class pradeep {
     public static void main(String[] args) {
-       int x=9;
-        System.out.println(x);
+
+        // int n=5;
+        // for(int i = 1;i<=n;i++){
+        // for(int j = 1;j<=i;j++){
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+
+    //  int n=5;
+    //  for(int i =n;i>=1;i--){
+    //     for(int j =1;j<=i;j++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    //  }
+
+    // int n=5;
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=i;j++){
+    //         System.out.print(i+j);
+    //     }
+    //     System.out.println();
+    // }
+
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=n-i;j++){
+    //         System.out.print(" ");
+    //     }
+    //     for(int j =1;j<=i;j++){
+    //         System.out.print(j+" ");
+    //     }
+    //     System.out.println();
+    // }
+
+//     int n  = 5;
+// for(int i=n;i>=1;i--){
+//     System.out.print(i);
+// }
+
+// int n =5;
+// for(int i=0;i<n;i++){
+//     for(int j=0;i<n-i-1;j++){
+//         System.out.print(" ");
+//     }
+//     int value=1;
+//     for(int j =0;j<=i;j++){
+//         System.out.print(value +" ");
+//         value = value*(i-j)/(j+1);
+//     }
+//     System.out.println();
+// }
+int n = 5;
+for(int i=0;i<n;i++){
+    for(int j=0;j<n-i-1;j++){
+        System.out.print(" ");
     }
+    int value=1;
+    for(int j=0;j<=i;j++){
+        System.out.print(value +" ");
+        value = value*(i-j)/(j+1);
+    }
+    System.out.println();
 }
 
 
-// Operator is symbol that is used to perform operations. + - / * 
-
-types of Operator:- 
-// Unary Opearator                postfix:-  var++ , var-- /   prefix:-  --var,++var  ,, ~  ! 
-// arithmetic   Operator                     + - / * %
-// shift Opearator                <<  >>  >>>
-// relational/comparison operator  <,> , <= , >=, ==, !=
-// bitwise operator               bitwise AND , bitwise exclusive OR, bitwise inclusive OR,
-// logical operator               &&  ||   !
-// ternary Opearator              ? :
-// assignment operator            = += ,-=,/=,*=, %=, &=, ^=, |=, <<=,>>=,>>>=
-
-// Unary operator required only one operand. 
-// unary operator are used to perform various operations 
-// increment /deccrement  a value by one 
-// negating an expression
-// inverting the value of a boolean
-    */
-
-    // class p{
-    //     public static void main(String[] args) {
-            
-        //    int a =10;
-        //    int b= -10;
-        //    System.out.println(a+b);
-        //    System.out.println(a-b);
-        //    System.out.println(a*b);
-        //    System.out.println(a/b);
-
-        // System.out.println(10<<2);  // gn1 * 2^gn2
-        // System.out.println(5<<3); //  
-        // System.out.println(10>>2); // gn1 / 2^gn2
-        // System.out.println(5>>3);
-
-        // System.out.println(2==2);
-        
-
-
-    //     }
-    // }
-
-//   class pradeep {
-  
-//    int age;
-//    String name;
-
-//    pradeep(int a, String n){
-//     age = a;
-//     name= n;
-//    }
-//    void  display(){
-//     System.out.println("age is:- " + age + " " + "name is:- " + name);
-//    }
-
-  
-//     public static void main(String[] args) {
-//         pradeep obj =  new pradeep(20,"pradeep");
-//         obj.display();
-//     }
-         
-//     }
-
-// class pradeep{
-//     int age;
-//     String name;
-//     int id;
-
-//     pradeep(int a,String n){
-//         age = a;
-//         name= n;
-
-//     }
-//     pradeep(int a,String n,int i){
-//         age = a;
-//         name = n;
-//         id= i;
-//     }
-//     void display(){
-//     System.out.println("age is:- " + age + " " + "name is:- " + name + " id is:- "+ id);
-//     }
-//     public static void main(String[] args) {
-//         pradeep obj = new pradeep(24, "vijendra");
-//         pradeep obj1 = new pradeep(20, "pradeeep", 123);
-//         obj.display();
-//         obj1.display();
-//     }
-//  
-
-
-// Static keywords 
-// variable
-// method
-// blocks
-// nested class
-
-// class staticc{
-//   static  int age =22;
-// String name = "vijendra";
-// int id = 123;
-// public static void main(String[] args) {
-//     staticc obj = new staticc();
-//     obj.age = 23;
-//     System.out.println(obj.age);
-// }
-// }
-
-// class staticc{
-//   int age;
-//   String name;
-//  static  int id = 321;
-//  staticc(int a, String n){
-// age= a;
-// name =n;
-//  }
-//  void display(){
-//     System.out.println("name is:- " + name + " age is:- "+age + " id is:- " + id);
-//  }
-
-//   public static void main(String[] args) {
-//       staticc obj = new staticc(23,"vijendra");
-  
-//       obj.display();
-//   }
-//   }
-
-
-// class counter{
-//   static  int count =0;
-//     counter(){
-//         count++;
-//         System.out.println(count);
-//     }
-// public static void main(String[] args) {
-//     counter obj =  new counter();
-//     counter obj1 =  new counter();
-//     counter obj2 =  new counter();
-// }
-// }
-
-
-// class bache{
-//     int rollno;
-//     String name;
-//     static String college = "ABC";
-//     static void method(){
-//         college = "def";
-//     }
-
-//     bache(int r, String n){
-//         rollno = r;
-//         name = n;
-//     }
-//     void display(){
-//         System.out.println("rollnumber is:- " + rollno + " name:- "+ name + " college is:- " + college);
-//     }
-
-   
-// }
-
-// class av{
-//     public static void main(String[] args) {
-//         bache.method();
-//         bache obj = new bache(123, "vijendra");
-//         bache obj1 = new bache(1234, "vijendra1");
-//         obj.display();
-//         obj1.display();
-
-//     }
-// }
-
-// class solve{
-//     static int Triangle(int x){
-//         return x+x+x;
-//     }
-//     public static void main(String[] args) {
-//         System.out.println(solve.Triangle(5));
-//     }
-// }
-
-
-// class non{
-//     static int x = 23;
-//     public static void main(String[] args) {
-//         System.out.println(x);
-//     }
-// }
-
-//Static block
-// class blck{
-//     static{
-//         int x =9;
-//         System.out.println("its stastic block " + x);
-//     }
-//     public static void main(String[] args) {
-//         System.out.println("its main ");
-//     }
-// }
-
-// class without{
-//     static{
-//         System.out.println(" --its static block-- ");
-//     }
-//     System.exit(0);
-// }
-
-
-// this keyword 
-// state 
-// behaviour
-
-// class ghi{
-//     int rollno;
-//     String name;
-//     float fees;
-
-//     ghi(int rollno, String name, float fees){
-//        this.rollno = rollno;
-//        this.name = name;
-        
-//        this.fees = fees;  
-//     }
-
-//     void display(){
-//         System.out.println(rollno+" "+name+" "+fees);
-//     }
-//     public static void main(String[] args) {
-//         ghi obj = new ghi(123, "vj", 1000);
-//         obj.display();
-//     }
-// }
-
-// class ghi{
-//     void m(){
-//         System.out.println("hello m");
-//     }
-//     void n(){
-//         this.m();
-//         System.out.println("hello n");
-//     }
-//     public static void main(String[] args) {
-//         ghi obj = new ghi();
-//         // obj.m();
-//         obj.n();
-//     }
-// // }
-
-// class conc{
-//     conc(){
-//         this();
-//         System.out.println(" its con1");
-//     }
-//     conc(int x){
-    
-//         System.out.println(x);
-//     }
-//     public static void main(String[] args) {
-//         conc obj = new conc(12);
-
-//     }
-// }
-
-// class m{
-//     m getA(){
-//         return this;
-//     }
-//     void msg(){System.out.println("hello vj");
-//     }
-//     public static void main(String[] args) {
-//         new m().getA().msg();
-//     }
-// }
-
-// class ref{
-//     void m(){
-//         System.out.println(this);
-//     }
-//     public static void main(String[] args) {
-//         ref obj = new ref();
-//         obj.m();
-//     }
-// }
-
-
-// ------Inheritence------
-// class p{
-//     //data members
-//     int height = 6;
-// }
-
-// class child extends p{
-//     int age = 14;
-
-//     public static void main(String[] args) {
-//         child obj = new child();
-//         System.out.println(obj.height +" "+ obj.age);
-//     }
-// }
-
-// class p{
-//     //data members
-//     int height = 6;
-// }
-
-// class child extends p{
-//     int age = 14;
-// }
-
-// class child1 extends child{
-//     int length =12;
-    
-// }
-
-
-// class pradeeep{
-//     public static void main(String[] args) {
-//         child1 obj = new child1();
-//         System.out.println(obj.height +" "+ obj.age + " "+ obj.length);
-//     }
-// }
-
-// class p{
-//     //data members
-//     int height = 6;
-// }
-
-// class child extends p{
-//     int age = 14;
-// }
-
-// class child1 extends p{
-//     int length =12;
-    
-// }
-
-// class pradeeep{
-//     public static void main(String[] args) {
-//         child1 obj = new child1();
-//         System.out.println(obj.height + " "+ obj.length);
-
-//         child obj1 = new child();
-//         System.out.println(obj1.height +" "+ obj1.age );
-//     }
-// }
-
-//--------Aggregation-------------
-// class vj{
-
-//     // fields
-// }
-
-
-// class pradeep{
-//     int square(int n){
-//         return n*n;
-//     }
-// }
-
-
-// class area{
-//     double pi = 3.14;
-//     pradeep obj;
-
-//     double circle(int radius){
-//         obj =  new pradeep();
-//         int rsqr = obj.square(radius);
-//         return pi*rsqr;
-//     }
-// }
-
-// class lkj{
-//     public static void main(String[] args) {
-//         area a = new area();
-//         System.out.println(a.circle(5));
-//     }
-// }
-
-
-
-// class gh{
-//     static int add(int x, int y){
-//         return x+y;
-//     }
-//     static double add(double m, double n){
-//         return m+n;
-//     }
-
-//     public static void main(String[] args) {
-//         System.out.println(gh.add(2, 3));
-//         System.out.println(gh.add(2.1, 3.1));
-//     }
-// }
-
-
-// class parnt{
-//     void abc(){
-//         System.out.println("its parent class");
-//     }
-// }
-
-// class chld extends parnt{
-//     void abc(){
-//         System.out.println("its child class");
-//     }
-
-//     public static void main(String[] args) {
-//         chld obj = new chld();
-//         obj.abc();
-//     }
-// }
-
-
-// class ij{
-//     void run(){
-//         System.out.println("mukesh is running");
-//     }
-// }
-
-// class chld extends ij{
-
-
-//     public static void main(String[] args) {
-//         chld obj =  new chld();
-//         obj.run();
-//     }
-// }
-
-
-// class salary{
-//     int getintrest(){
-//         return 0;
-//     }
-// }
-
-// class kotak extends salary{
-//     int getintrest(){
-//         return 7;
-//     }
-// }
-
-// class SBI extends salary{
-//     int getintrest(){
-//         return 8;
-//     }
-// }
-
-// class pradeeep{
-//     public static void main(String[] args) {
-//         kotak obj = new kotak();
-//         SBI obj1 = new SBI();
-//         System.out.println(obj.getintrest());
-//         System.out.println(obj1.getintrest());
-
-
-//     }
-// }
-
-
-// class pradeeep{
-//     pradeeep abc(){
-//         return this; 
-//     }
-//     void display(){
-//         System.out.println(" its covriant return type");
-
-//     }
-// }
-
-// class chld extends pradeeep{
-//     chld abc(){
-//         return this;
-//     }
-
-//     // void display(){
-//     //     System.out.println(" its covriant return type");
-
-//     // }
-//     public static void main(String[] args) {
-//         new chld().abc().display();
-//     }
-// }
+    }
+}
